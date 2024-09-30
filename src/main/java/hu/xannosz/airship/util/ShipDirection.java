607e05d2e58c4bb95aaa5b3fc0976f38b7fef849
Xannosz <kiss.andras.zoltan@gmail.com>
2024-10-01 00:09:55 +0200

    Add direction text

--- src/main/java/hu/xannosz/airship/util/ShipDirection.java
+++ src/main/java/hu/xannosz/airship/util/ShipDirection.java
@@ -6,15 +6,16 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum ShipDirection {
-	N(0, 0, -1, 0F), E(1, 1, 0, 90F), S(2, 0, 1, 180F), W(3, -1, 0, 270F),
-	NE(4, 0.707f, -0.707f, 45F), NW(5, -0.707f, -0.707f, 315F), SE(6, 0.707f, 0.707f, 135F), SW(7, -0.707f, 0.707f, 225F),
-	NNE(8, 0.383f, -0.924f, 22.5F), ENE(9, 0.924f, -0.383f, 67.5F), NNW(10, -0.383f, -0.924f, 337.5F), WNW(11, -0.924f, -0.383f, 292.5F),
-	SSE(12, 0.383f, 0.924f, 157.5F), ESE(13, 0.924f, 0.383f, 112.5F), SSW(14, -0.383f, 0.924f, 202.5F), WSW(15, -0.924f, 0.383f, 247.5F);
+	N(0, 0, -1, 0F, "Z--"), E(1, 1, 0, 90F, "X++"), S(2, 0, 1, 180F, "Z++"), W(3, -1, 0, 270F, "X--"),
+	NE(4, 0.707f, -0.707f, 45F, "X+ Z-"), NW(5, -0.707f, -0.707f, 315F, "X- Z-"), SE(6, 0.707f, 0.707f, 135F, "X+ Z+"), SW(7, -0.707f, 0.707f, 225F, "X- Z+"),
+	NNE(8, 0.383f, -0.924f, 22.5F, "X+ Z--"), ENE(9, 0.924f, -0.383f, 67.5F, "X++ Z-"), NNW(10, -0.383f, -0.924f, 337.5F, "X- Z--"), WNW(11, -0.924f, -0.383f, 292.5F, "X-- Z-"),
+	SSE(12, 0.383f, 0.924f, 157.5F, "X+ Z++"), ESE(13, 0.924f, 0.383f, 112.5F, "X++ Z+"), SSW(14, -0.383f, 0.924f, 202.5F, "X- Z++"), WSW(15, -0.924f, 0.383f, 247.5F, "X-- Z+");
 
 	private final int code;
 	private final float x;
 	private final float z;
 	private final float angel;
+	private final String name;
 
 	public ShipDirection right() {
 		switch (this) {
